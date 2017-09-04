Instead of comparing normalized gradient to a fixed threshold taking as a probability of moving along an axis. Explored voxels are now indicated by a delta function which persistent over time meaning it remembers which voxels has been visited and how often it has been visited. The avoid collision has been added as a gaussian distribution with mean at the current position of the UAV. Mock temperature has been introduced. More work to do to adapt it with actual drone with real temperature sensor.  -Mahi

scripts/phi.py
@@ -5,7 +5,7 @@ import rospy
 from geometry_msgs.msg import Pose, Point, Quaternion, Twist, Vector3
 from tf.transformations import quaternion_from_euler
 from rospy.numpy_msg import numpy_msg
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32
 from cloud_map.msg import Belief
 
 
@@ -19,6 +19,7 @@ class Unexplored(object):
         self._q_size = q_size
         self._q_pose = []  # type: list[np.ndarray]
         self._phi_unexplored = np.zeros(self._space)
+        self._contour_scale = .3
 
     def callback_pose(self, pose):
         """
@@ -26,7 +27,7 @@ class Unexplored(object):
         :return: 
         """
         dist = multivariate_normal(mean=np.array(pose.position.__getstate__()[:self._dim], dtype='int'),
-                                   cov=self._scale * np.identity(self._dim))
+                                   cov=(self._scale * self._contour_scale) * np.identity(self._dim))
         indices = np.ndindex(self._space)
         last = np.array(
             map(lambda x: dist.pdf(np.array(x[:])), indices), dtype=np.float32).reshape(self._space)
@@ -34,8 +35,9 @@ class Unexplored(object):
         self._q_pose.append(last)
         self._q_pose = self._q_pose[-self._q_size:]
         self._phi_unexplored = np.zeros(self._space)
-        for p in self._q_pose:
-                self._phi_unexplored += p
+        decay = np.linspace(start=0.6, stop=1., num=len(self._q_pose))
+        for i in range(len(self._q_pose)):
+            self._phi_unexplored += self._q_pose[i] * decay[i]
         self._phi_unexplored /= np.sum(self._phi_unexplored)
 
     def start(self):
@@ -60,5 +62,58 @@ def phi_unexplored_node(name):
         if "unexplored" in intents.split('_'):
             phi_unexplored = Unexplored(name=name, dim=dim, scale=scale)
             phi_unexplored.start()
+
+
+class MockTemperature(object):
+    def __init__(self, name, dim, scale, q_size=60):
+        self._name = name
+        self._dim = dim
+        self._scale = scale
+        self._space = tuple([scale for _ in range(dim)])
+        self._dim = len(self._space)
+        self._measured_temp = 50. * np.zeros(self._space)
+        self._phi_temp_change = np.zeros(self._space)
+        self._pose = Pose()
+        self._tag = "[Temp {}]".format(self._name)
+
+    def callback_sensor_pose_euclid(self, pose): self._pose = pose
+
+    def callback_mock_temp(self, temp):
+        """
+        :type temp: Float32
+        :return:
+        """
+        x, y, z = map(int, np.array(self._pose.position.__getstate__())[:])
+        self._measured_temp[x, y, z] = temp
+        # calc temp change
+        self._phi_temp_change = np.abs(self._measured_temp - temp)
+        self._phi_temp_change /= np.sum(self._phi_temp_change)
+        self._phi_temp_change = 1. - self._phi_temp_change
+        rospy.logdebug("{} is {}".format(self._tag, temp))
+
+    def start(self):
+        rospy.init_node(self._name, log_level=rospy.DEBUG)
+        rate = rospy.Rate(4)
+        rospy.Subscriber("/Sensors/" + self._name + "/mock_temperature", Float32, callback=self.callback_mock_temp)
+        rospy.Subscriber("/solo/{}/pose_euclid".format(self._name), Pose, callback=self.callback_sensor_pose_eulid)
+
+        pub_temp_change = rospy.Publisher(self._name + '/temp_change', numpy_msg(Belief), queue_size=10.)
+        while not rospy.is_shutdown():
+            msg = Belief()
+            msg.header.frame_id = self._name
+            msg.header.stamp = rospy.Time.now()
+            msg.data = self._phi_temp_change.ravel()
+            pub_temp_change.publish(msg)
+            rate.sleep()
+
+
+def phi_mock_temperature_node(name):
+    dim = int(rospy.get_param("/dim"))
+    scale = int(rospy.get_param("/scale"))
+    if rospy.has_param("/PHI/" + name + "/intent"):
+        intents = rospy.get_param("/PHI/" + name + "/intent")  # type: str
+        if "mocktemperature" in intents.split('_'):
+            phi_mock_temp = MockTemperature(name=name, dim=dim, scale=scale)
+            phi_mock_temp.start()
 
 

scripts/solo_2d.py
@@ -24,14 +24,25 @@ class solo_2d(object):
         self._tag = "[solo_{}]".format(port)
         self._goal_gps = Pose(Point(0., 0., 0.), Quaternion(*quaternion_from_euler(0., 0., 0.)))
         self._goal_euclid = Pose(Point(0., 0., 0.), Quaternion(*quaternion_from_euler(0., 0., 0.)))
-        self._dim = 2
+        self._dim = int(rospy.get_param("/dim"))
+        self._scale = int(rospy.get_param("/scale"))
         # cowboy cricket ground bowling end 36.133642, -97.076528
-        self._origin_lat = 36.133642
-        self._origin_lon = -97.076528
-        self._alt = 8
-        self._meters_per_disposition = 2.
+        # self._origin_lat = 36.133642
+        # self._origin_lon = -97.076528
+        # # campus
+        # self._origin_lat = 36.1214838
+        # self._origin_lon = -97.0698584
+        # UAFS site corner 36.1620828,-96.8358856
+        self._origin_lat = 36.1620828
+        self._origin_lon = -96.8358856
+        self._log_file = open("log_poses_{}_{}.txt".format(self._tag, time.time()), mode='a+')
+        self._origin_alt = 10.  # meter
+        self._goal_alt = 20
+        self._meters_per_alt = 1.
+        self._meters_per_disposition = 2.5
         self._meters_per_lat = 110961.03  # meters per degree of latitude for use near Stillwater
         self._meters_per_lon = 90037.25  # meters per degree of longitude
+
         try:
             self._vehicle = dronekit.connect("udpin:0.0.0.0:{}".format(port))
         except Exception as e:
@@ -41,17 +52,15 @@ class solo_2d(object):
         self._pub_pose_euclid = None
         self._pub_distance_to_goal = None
 
-    def arm_and_takeoff(self, aTargetAltitude=10, starting_coordinate=None):
+    def arm_and_takeoff(self, start_at_euclid=None):
         """
         Init ROS node.
         Arms vehicle and fly to aTargetAltitude (in meters).
         """
-        rospy.init_node(self._name, log_level=rospy.DEBUG)
-        rate = rospy.Rate(1)
-        rospy.logdebug("{}Basic pre-arm checks".format(self._tag))
+        print("{}Basic pre-arm checks".format(self._tag))
         # Don't try to arm until autopilot is ready
         while not self._vehicle.is_armable:
-            rospy.logdebug("{}Waiting for vehicle to initialise...".format(self._tag))
+            print("{}Waiting for vehicle to initialise...".format(self._tag))
             time.sleep(1)
 
         rospy.logdebug("{}Arming motors".format(self._tag))
@@ -61,17 +70,36 @@ class solo_2d(object):
 
         # Confirm vehicle armed before attempting to take off
         while not self._vehicle.armed:
-            rospy.logdebug("{}Waiting for arming...".format(self._tag))
+            print("{}Waiting for arming...".format(self._tag))
             time.sleep(1)
 
-        self._alt = aTargetAltitude
-        rospy.logdebug("Taking off to{}".format(self._alt))
-        self._vehicle.simple_takeoff(self._alt)
+        print("{}Taking off to {}m".format(self._tag, self._origin_alt))
+        self._vehicle.simple_takeoff(self._origin_alt)
+
+        # give solo time to take off
         time.sleep(10)
+        self._goal_euclid = Pose(Point(start_at_euclid[0], start_at_euclid[1], self._origin_alt + self._meters_per_alt * start_at_euclid[2]), Quaternion(*quaternion_from_euler(0., 0., 0.)))
+        lat, lon = self.euclid_to_geo(NS=self._goal_euclid.position.y, EW=self._goal_euclid.position.x)
+        # longitude EW = x axis and latitude NS = y axis
+        self._goal_gps.position.x = lon
+        self._goal_gps.position.y = lat
+        self._goal_gps.position.z = self._origin_alt + self._meters_per_alt * self._goal_euclid.position.z
+        # send solo to initial location
+        self._vehicle.simple_goto(dronekit.LocationGlobalRelative(lat=self._goal_gps.position.y, lon=self._goal_gps.position.x,
+                                                                  alt=self._goal_gps.position.z))
+        print("{}Sending to initial goal (x,y,z)=({}) (lon, lat, alt)=({},{},{})".
+              format(self._tag, start_at_euclid, lon, lat, self._origin_alt + self._meters_per_alt * start_at_euclid[2]))
+
+        time.sleep(5)
+
+        rospy.init_node(self._name, log_level=rospy.DEBUG)
+        rospy.logdebug("{} init node".format(self._tag))
+        rate = rospy.Rate(1)
 
         rospy.Subscriber("/UAV/{}/next_way_point_euclid".format(self._name), data_class=Pose,
                          callback=self.callback_go_to_next_euclidean_way_point)
         rospy.Subscriber("/UAV/{}/land".format(self._name), data_class=String, callback=self.callback_land)
+
         self._pub_pose_gps = rospy.Publisher(self._name + '/pose_gps', data_class=Pose, queue_size=10)
         self._pub_pose_euclid = rospy.Publisher(self._name + '/pose_euclid', data_class=Pose, queue_size=10)
         self._pub_distance_to_goal = rospy.Publisher(self._name + '/distance_from_goal', data_class=Float32, queue_size=10)
@@ -79,6 +107,7 @@ class solo_2d(object):
             pose_gps = Pose()
             pose_gps.position.x = self._vehicle.location.global_relative_frame.lon
             pose_gps.position.y = self._vehicle.location.global_relative_frame.lat
+            pose_gps.position.z = self._vehicle.location.global_relative_frame.alt
             self._pub_pose_gps.publish(pose_gps)
             self._pub_pose_euclid.publish(self.pose_in_euclid())
             self._pub_distance_to_goal.publish(self.get_distance_to_goal())
@@ -109,8 +138,18 @@ class solo_2d(object):
         pose = Pose()
         pose.position.x = (self._vehicle.location.global_relative_frame.lon - self._origin_lon) * self._meters_per_lon / self._meters_per_disposition
         pose.position.y = (self._vehicle.location.global_relative_frame.lat - self._origin_lat) * self._meters_per_lat / self._meters_per_disposition
-        rospy.logdebug("{}Pose (x,y)=({},{}) actual (lon,lat)=({},{})".format(self._tag, pose.position.x, pose.position.y,
-                                                                         self._vehicle.location.global_relative_frame.lon, self._vehicle.location.global_relative_frame.lat))
+        pose.position.z = (self._vehicle.location.global_relative_frame.alt - self._origin_alt) / self._meters_per_alt
+        rospy.logdebug("{}Pose (x,y,z)=({},{},{}) actual (lon,lat,alt)=({},{},{})"
+                       .format(self._tag, pose.position.x, pose.position.y, pose.position.z,
+                       self._vehicle.location.global_relative_frame.lon,
+                       self._vehicle.location.global_relative_frame.lat,
+                       self._vehicle.location.global_relative_frame.alt))
+        self._log_file.write("{}_{}_ {},{},{} {},{},{}\n".format(self._tag, rospy.Time.now(), self._vehicle.location.global_relative_frame.lon,
+                                                      self._vehicle.location.global_relative_frame.lat,
+                                                      self._vehicle.location.global_relative_frame.alt, pose.position.x, pose.position.y, pose.position.z))
+        if pose.position.x < -1. or pose.position.y < -1 or pose.position.x > self._scale or pose.position.y > self._scale:
+            rospy.logdebug("{} landing because went out of boundary!!! ".format(self._tag))
+            self._vehicle.mode = dronekit.VehicleMode("LAND")
         return pose
 
     def callback_go_to_next_euclidean_way_point(self, goal_euclid):
@@ -119,14 +158,20 @@ class solo_2d(object):
         :type goal_euclid: Pose
         :return:
         """
+        if goal_euclid is None:
+            rospy.logdebug("{} No goal waypoint received yet.".format(self._tag))
+            return
+
         self._goal_euclid = goal_euclid
         lat, lon = self.euclid_to_geo(NS=goal_euclid.position.y, EW=goal_euclid.position.x)
-        # longitude EW = x axis and latitude NS = y axis
+        # longitude EW = x axis and latitude NS = y axis, E is +x, N is +y
         self._goal_gps.position.x = lon
         self._goal_gps.position.y = lat
-        rospy.logdebug("{}Going to waypoint x={} y={} lat={} long={}".format(self._tag,goal_euclid.position.x, goal_euclid.position.y, lat, lon))
+        self._goal_gps.position.z = self._origin_alt + self._meters_per_alt * goal_euclid.position.z
+        rospy.logdebug("{}Going to waypoint (x,y,z)=({},{},{}) lat={} long={} alt={}".
+                       format(self._tag,goal_euclid.position.x, goal_euclid.position.y, goal_euclid.position.z, lat, lon, self._goal_gps.position.z))
         self._vehicle.simple_goto(dronekit.LocationGlobalRelative(self._goal_gps.position.y, self._goal_gps.position.x,
-                                                                  self._alt))
+                                                                  self._goal_gps.position.z))
 
     def callback_land(self, msg):
         rospy.logdebug("{}Returning to Launch".format(self._tag))
@@ -136,14 +181,26 @@ class solo_2d(object):
         rospy.logdebug("{}Close vehicle object".format(self._tag))
         self._vehicle.close()
 
+        self._log_file.close()
+
     def get_distance_to_goal(self):
         pose = self.pose_in_euclid()
-        d = np.sqrt((self._goal_euclid.position.x - pose.position.x)**2. +
-                       (self._goal_euclid.position.y - pose.position.y)**2.)
-        if d < 0.5:
-            rospy.logdebug("{}reached goal (long,lat)=({},{}) (x,y)=({},{})".format(
+        if self._dim == 3:
+            d = np.sqrt((self._goal_euclid.position.x - pose.position.x)**2. + (self._goal_euclid.position.y - pose.position.y)**2. + (self._goal_euclid.position.z - pose.position.z)**2.)
+        if self._dim == 2:
+            d = np.sqrt((self._goal_euclid.position.x - pose.position.x)**2. + (self._goal_euclid.position.y - pose.position.y)**2.)
+        rospy.logdebug("{}goal_gps={} dz_gps={} scaled_alt={} actual_alt={} d={}".format(self._tag, self._goal_gps.position.z,
+                        (self._goal_gps.position.z - self._vehicle.location.global_relative_frame.alt)**2.,
+                        pose.position.z,
+                        self._vehicle.location.global_relative_frame.alt, d))
+        tol = 0.5
+        if self._dim == 2: tol = 0.5
+        if self._dim == 3: tol = 0.8
+
+        if d < tol:
+            rospy.logdebug("{}reached goal (long,lat,alt)=({},{},{}) (x,y)=({},{},{})".format(
                 self._tag, self._vehicle.location.global_relative_frame.lon,
-                self._vehicle.location.global_relative_frame.lat, pose.position.x, pose.position.y))
+                self._vehicle.location.global_relative_frame.lat, self._vehicle.location.global_relative_frame.alt, pose.position.x, pose.position.y, pose.position.z))
         return d
 
 

scripts/uav.py
@@ -23,7 +23,7 @@ class dummy_uav(object):
         self._pose = Pose(Point(0., 0., 0.), Quaternion(*quaternion_from_euler(0., 0., 0.)))
         self._vel = Twist(Vector3(1., 0., 0.), Vector3(0., 0., 0.))
         self._stall_vel = 1.0
-        self._defined_intention_keys = ["unexplored"]
+        self._defined_intention_keys = ["unexplored", "tempchange"]
         self._decay_explored = .3
         self._decay_belief = .3
         self._belief = np.ones(shape=self._space) / (scale**self._dim)
@@ -84,6 +84,17 @@ class dummy_uav(object):
             map(lambda x: dist.pdf(np.array(x[:])), indices), dtype=np.float32).reshape(self._space)
         return self._belief
 
+    @property
+    def defined_intention_keys(self):
+        """
+        :rtype : list[str]
+        """
+        return self._defined_intention_keys
+
+    @defined_intention_keys.setter
+    def defined_intention_keys(self, value):
+        self._defined_intention_keys = value
+
     def sum_product_algo2(self):
         """
         Algo 2 is taking sum instead of product.
@@ -108,10 +119,19 @@ class dummy_uav(object):
         new_intention -= self.belief_position
         res = new_intention / np.sum(new_intention)
 
-        X, Y = np.meshgrid(np.arange(0, self._scale, 1.), np.arange(0, self._scale, 1.))
-        wall = (X-self._scale/2)**4 + (Y-self._scale/2)**4
-        wall /= np.sum(wall)
-        wall *= 0.1
+        if self._dim == 2:
+            X, Y = np.meshgrid(np.arange(0, self._scale, 1.), np.arange(0, self._scale, 1.))
+            wall = (X-self._scale/2)**4 + (Y-self._scale/2)**4
+            wall /= np.sum(wall)
+            wall *= 0.05
+
+        if self._dim == 3:
+            # X, Y, Z = np.meshgrid(np.arange(0, self._scale, 1.), np.arange(0, self._scale, 1.), np.arange(0, self._scale, 1.))
+            # wall = (X-self._scale/2)**4. + (Y-self._scale/2)**4. + (Z-self._scale/2)**4.
+            wall = np.zeros(shape=(self._scale, self._scale, self._scale))
+            wall[:, :, 0] = wall[:, 0, :] = wall[0, :, :] = wall[:, :, self._scale-1] = wall[:, self._scale-1, :] = wall[self._scale-1, :, :] = 1.
+            wall /= np.sum(wall)
+            wall *= 0.3
 
         self._intention_fusion = np.array(res.reshape(self._space), dtype=np.float32) + wall
 
@@ -128,26 +148,19 @@ class dummy_uav(object):
             gradient_at_cur_pos = np.array(np.gradient(1.-self._intention_fusion), dtype=np.float32)[:, x, y, z]
 
         k = np.sqrt(np.sum(gradient_at_cur_pos**2.))
-        scaled_grad = gradient_at_cur_pos/k
+        scaled_grad = 1.5 * gradient_at_cur_pos/k
 
         old_pos = self.position
         goal = Pose()
+
         if not np.isclose(k, 0., atol=1.e-12):
-            threshold = 0.1
-            noisy_unit = (3. + np.random.uniform(low=-1., high=1.) * 0.00001)
-            if scaled_grad[0] > (threshold) and self.pose.position.x + noisy_unit < self._scale:
-                goal.position.x = self.pose.position.x + noisy_unit
-            if scaled_grad[1] > (threshold) and self.pose.position.y + noisy_unit < self._scale:
-                goal.position.y = self.pose.position.y + noisy_unit
-            if scaled_grad[0] < (-1.*threshold) and self.pose.position.x > noisy_unit - 1.:
-                goal.position.x = self.pose.position.x - noisy_unit
-            if scaled_grad[1] < (-1.*threshold) and self.pose.position.y > noisy_unit - 1.:
-                goal.position.y = self.pose.position.y - noisy_unit
+            if 0. <= self.pose.position.x + scaled_grad[0] < self._scale - 1.:
+                goal.position.x = self.pose.position.x + scaled_grad[0]
+            if 0. <= self.pose.position.y + scaled_grad[1] < self._scale - 1.:
+                goal.position.y = self.pose.position.y + scaled_grad[1]
             if self._dim == 3:
-                if scaled_grad[2] > (threshold) and self.pose.position.z + noisy_unit < self._scale:
-                    goal.position.z = self.pose.position.z + noisy_unit
-                if scaled_grad[2] < (-1.*threshold) and self.pose.position.z > noisy_unit - 1:
-                    goal.position.z = self.pose.position.z - noisy_unit
+                if 0. <= self.pose.position.z + scaled_grad[2] < self._scale - 1.:
+                    goal.position.z = self.pose.position.z + scaled_grad[2]
 
         # rospy.logdebug("{}:occupancy\n{}".format(self.name, self._intention_fusion))
         rospy.logdebug("[{}]{}:{}->{} grad {}|{} 0?{}".format(dt.datetime.fromtimestamp(rospy.Time.now().to_time()).strftime("%M:%S.%f"), self.name, old_pos, np.array([goal.position.x, goal.position.y, goal.position.z]), gradient_at_cur_pos, scaled_grad, np.isclose(k, 0.)))
@@ -182,15 +195,24 @@ class dummy_uav(object):
         # :type pdf_unexplored: Belief
         self._phi["unexplored"] = np.asanyarray(pdf_unexplored.data, dtype=np.float32).reshape(self._space)
 
-    def take_off(self, start_at):
+    def callback_phi_temp_change(self, pdf_unexplored):
+        # :type pdf_unexplored: Belief
+        self._phi["tempchange"] = np.asanyarray(pdf_unexplored.data, dtype=np.float32).reshape(self._space)
+
+    def take_off(self):
         """
         uav rosnode launcher and intention publisher
         """
+
+        choice = raw_input("Start autonomous node? Press any letter").lower()
+        print("choice = ", choice)
+        print("Starting autonomouse mode......")
         rospy.init_node(self._name, log_level=rospy.DEBUG)
         rate = rospy.Rate(2)
         rospy.Subscriber("/solo/{}/pose_euclid".format(self._name), Pose, callback=self.callback_sensor_pose)
         rospy.Subscriber("/solo/{}/distance_from_goal".format(self._name), Float32, callback=self.callback_goal_reached)
         rospy.Subscriber("/PHI/{}/unexplored".format(self.name), numpy_msg(Belief), callback=self.callback_phi_unexplored)
+        rospy.Subscriber("/PHI/{}/temp_change".format(self.name), numpy_msg(Belief), callback=self.callback_phi_temp_change)
         for from_uav in self.neighbour_names:
             topic = "/UAV/" + from_uav + "/intention_sent"
             rospy.Subscriber(topic, Belief, callback=self.callback)
@@ -200,11 +222,6 @@ class dummy_uav(object):
             init_belief.header.stamp = rospy.Time.now()
             init_belief.data = np.ones(self._scale ** self._dim, dtype=np.float32).ravel()
             self._msg_received[from_uav] = init_belief
-        self._pose = Pose(Point(start_at[0], start_at[1], start_at[2]), Quaternion(*quaternion_from_euler(0., 0., np.pi)))
-
-        time.sleep(15)
-        rospy.logdebug("{} sending to inital goal (x,y)=({}, {})!!!".format(self.name, self._pose.position.x, self._pose.position.y))
-        self._pub_goal_euclid.publish(self._pose)
 
         q_size = 10
         # fix3d
@@ -240,11 +257,12 @@ class dummy_uav(object):
             rate.sleep()
 
 
-def launch_uav(name,start_at):
+def launch_uav(name):
     # default scale 2 will be overwritten by rosparam space
     dim = int(rospy.get_param("/dim"))
     scale = int(rospy.get_param("/scale"))
     uav = dummy_uav(name=name, dim=dim, scale=scale)
+    uav.defined_intention_keys = rospy.get_param("/PHI/" + name + "/intent").split('_')
 
     neighbors = []
     if rospy.has_param("/UAV/" + name + "/neighbors"):
@@ -255,4 +273,4 @@ def launch_uav(name,start_at):
             uav.neighbour_names.append(n)
 
     print("UAV_2D launcing {} with neighbors {}".format(name, neighbors))
-    uav.take_off(start_at=start_at)
+    uav.take_off()

scripts/visualization_2d_contour.py
@@ -80,9 +80,10 @@ class Visualization(object):
         # anims.append(animation.FuncAnimation(
         #     fig, self.update_self_viz, 1000, fargs=(unused, ax1self, ax2cax1), interval=50, blit=False))
         ind = 0
+        interval = 500
         anims = [None, None, None, None, None]
         anims[ind] = animation.FuncAnimation(
-            fig, self.update_self_viz, 1000, fargs=(unused, ax1self, ax2cax1), interval=1000, blit=False)
+            fig, self.update_self_viz, 1000, fargs=(unused, ax1self, ax2cax1), interval=interval, blit=False)
 
         if len(self.neighbor_names) > 0:
             for to_uav, ax_sent, cax in zip(self.neighbor_names, [ax5sent1, ax6sent2], [ax2cax5, ax2cax6]):
@@ -93,7 +94,7 @@ class Visualization(object):
                 #                                      interval=50, blit=False))
                 ind += 1
                 anims[ind] = animation.FuncAnimation(
-                    fig, self.update_sent_viz, 1000, fargs=(unused, ax_sent, to_uav, cax), interval=1000, blit=False)
+                    fig, self.update_sent_viz, 1000, fargs=(unused, ax_sent, to_uav, cax), interval=interval, blit=False)
 
             for from_uav, ax_rec, cax in zip(self.neighbor_names, [ax3recv1, ax4recv2], [ax2cax3, ax2cax4]):
                 # anim3 = animation.FuncAnimation(fig, self.update_received_viz, 1000,
@@ -103,7 +104,7 @@ class Visualization(object):
                 #                                      fargs=(unused, ax_rec, from_uav, cax), interval=50, blit=False))
                 ind += 1
                 anims[ind] = animation.FuncAnimation(
-                    fig, self.update_received_viz, 1000, fargs=(unused, ax_rec, from_uav, cax), interval=1000, blit=False)
+                    fig, self.update_received_viz, 1000, fargs=(unused, ax_rec, from_uav, cax), interval=interval, blit=False)
 
         plt.suptitle("Robot {}'s Perspective-{}".format(self.name,dt.datetime.fromtimestamp(rospy.Time.now().to_time()).strftime("%M:%S.%f")), fontsize=10)
         # plt.subplots_adjust(wspace=0, hspace=0)
@@ -123,10 +124,10 @@ class Visualization(object):
         ax.clear()
         # Setting the axes properties
         ax.set_xlim([0.0, float(self.scale)])
-        ax.set_xlabel('X')
+        ax.set_xlabel('Y')
 
         ax.set_ylim([0.0, float(self.scale)])
-        ax.set_ylabel('Y')
+        ax.set_ylabel('X')
 
         ax.set_xticks([])
         ax.set_yticks([])
@@ -194,10 +195,10 @@ class Visualization(object):
         ax.clear()
         # Setting the axes properties
         ax.set_xlim([0.0, float(self.scale)])
-        ax.set_xlabel('X')
+        ax.set_xlabel('Y')
 
         ax.set_ylim([0.0, float(self.scale)])
-        ax.set_ylabel('Y')
+        ax.set_ylabel('X')
 
         ax.set_xticks([])
         ax.set_yticks([])
@@ -207,8 +208,8 @@ class Visualization(object):
         ax.text(self.pose[1], self.pose[0], self.name)
         for nm in self.neighbor_names:
             if self._pose_received.has_key(nm):
-                p3 = self._pose_received[nm]
-                ax.text(p3[1], p3[0], nm)
+                p2 = self._pose_received[nm]
+                ax.text(p2[1], p2[0], nm)
 
         if self._intention_received.has_key(from_uav):
             ax.set_title("{}<{}#{}-T1-{}-T0-{}".format(self.name, from_uav, num,dt.datetime.fromtimestamp(rospy.Time.now().to_time()).strftime("%M:%S.%f"),
@@ -235,29 +236,29 @@ class Visualization(object):
         :type pdf_intention: Belief
         """
         # tmp = np.asanyarray(pdf_intention.data).reshape(self.scale, self.scale)
-        self._intention_sent[pdf_intention.header.frame_id[2]] = pdf_intention #tmp
+        self._intention_sent[pdf_intention.header.frame_id[2]] = pdf_intention  # tmp
 
     def callback_intention_received(self, pdf_intention):
         """
         :type pdf_intention: Belief
         """
         # tmp = np.asanyarray(pdf_intention.data).reshape(self.scale, self.scale)
-        self._intention_received[pdf_intention.header.frame_id] = pdf_intention #tmp
+        self._intention_received[pdf_intention.header.frame_id] = pdf_intention  # tmp
 
     def callback_intention_self(self, pdf_intention):
         """
         :type pdf_intention: Belief
         """
         # tmp = np.asanyarray(pdf_intention.data).reshape(self.scale, self.scale)
-        self._intention_self = pdf_intention #tmp
+        self._intention_self = pdf_intention  # tmp
 
 
 def visualizer(name):
     # delete previous output plots
     cmd = "rm /home/alien/catkin_ws/src/cloud_map/scripts/frames{}/*".format(name)
     os.system(cmd)
-    cmd = "rm /home/alien/catkin_ws/src/cloud_map/scripts/gifs/*".format(name)
-    os.system(cmd)
+    # cmd = "rm /home/alien/catkin_ws/src/cloud_map/scripts/gifs/*".format(name)
+    # os.system(cmd)
     # default scale 2 will be overwritten by rosparam space
     scale = int(rospy.get_param("/scale"))
     viz = Visualization(name=name, d=scale)
@@ -265,7 +266,7 @@ def visualizer(name):
         neighbours = rospy.get_param("/Visual/" + name + "/neighbors").split("_")
         for n in neighbours:
             # todo validate the format of n
-            if n!= '':
+            if n != '':
                 viz.neighbor_names.append(n)
     rospy.logdebug("{} Visual wait for 12 sec...")
     time.sleep(12)